f012e3b713676a926365c5b1fb1c252d871eee1b
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-04-14 20:31:04 +0530

    Testing for Session constructor
    As code for constructor is added, it should be checked as well. But the class is
    not completely implemented yet. As more options are added, the tests will be
    expanded.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- src/test/java/org/arithmetic/SessionTest.java
+++ src/test/java/org/arithmetic/SessionTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.arithmetic.OperatorList;
 import org.arithmetic.Session;
+import org.arithmetic.defaults.Defaults;
 import org.arithmetic.exception.AppException;
 
 public class SessionTest {
@@ -78,5 +79,136 @@ public class SessionTest {
         Session s = new Session();
         s.setOpList(op);
     }
+
+    @Test
+    public void emptyConstructorTest() throws AppException {
+        Session s = new Session();
+
+        short expNumQues = Defaults.NUM_QUES;
+        int expMaxRange = Defaults.MAX_RANGE;
+        String expOpStr = Defaults.DEF_OPS;
+
+        short testNumQues = s.getNumQues();
+        int testMaxRange = s.getMaxRange();
+        String testOpStr = s.getOpList();
+
+        assertEquals(expNumQues, testNumQues);
+        assertEquals(expMaxRange, testMaxRange);
+        assertEquals(expOpStr, testOpStr);
+    }
+
+    @Test
+    public void numConstructorTest() throws AppException {
+        short numQues = 500;
+        Session s = new Session(numQues);
+
+        short expNumQues = numQues;
+        int expMaxRange = Defaults.MAX_RANGE;
+        String expOpStr = Defaults.DEF_OPS;
+
+        short testNumQues = s.getNumQues();
+        int testMaxRange = s.getMaxRange();
+        String testOpStr = s.getOpList();
+
+        assertEquals(expNumQues, testNumQues);
+        assertEquals(expMaxRange, testMaxRange);
+        assertEquals(expOpStr, testOpStr);
+    }
+
+    @Test
+    public void rangeConstructorTest() throws AppException {
+        int maxRange = 500;
+        Session s = new Session(maxRange);
+
+        short expNumQues = Defaults.NUM_QUES;
+        int expMaxRange = maxRange;
+        String expOpStr = Defaults.DEF_OPS;
+
+        short testNumQues = s.getNumQues();
+        int testMaxRange = s.getMaxRange();
+        String testOpStr = s.getOpList();
+
+        assertEquals(expNumQues, testNumQues);
+        assertEquals(expMaxRange, testMaxRange);
+        assertEquals(expOpStr, testOpStr);
+    }
+
+    @Test
+    public void numRangeConstructorTest() throws AppException {
+        short numQues = 500;
+        int maxRange = 600;
+        Session s = new Session(numQues, maxRange);
+
+        short expNumQues = numQues;
+        int expMaxRange = maxRange;
+        String expOpStr = Defaults.DEF_OPS;
+
+        short testNumQues = s.getNumQues();
+        int testMaxRange = s.getMaxRange();
+        String testOpStr = s.getOpList();
+
+        assertEquals(expNumQues, testNumQues);
+        assertEquals(expMaxRange, testMaxRange);
+        assertEquals(expOpStr, testOpStr);
+    }
+
+    @Test
+    public void rangeNumConstructorTest() throws AppException {
+        short numQues = 500;
+        int maxRange = 600;
+        Session s = new Session(maxRange, numQues);
+
+        short expNumQues = numQues;
+        int expMaxRange = maxRange;
+        String expOpStr = Defaults.DEF_OPS;
+
+        short testNumQues = s.getNumQues();
+        int testMaxRange = s.getMaxRange();
+        String testOpStr = s.getOpList();
+
+        assertEquals(expNumQues, testNumQues);
+        assertEquals(expMaxRange, testMaxRange);
+        assertEquals(expOpStr, testOpStr);
+    }
+
+    @Test
+    public void byteConstrTest() throws AppException {
+        // A byte argument would be cast into short constructor.
+        byte numQues = 56;
+
+        Session s;
+        short testQues, expQues;
+
+        s = new Session(numQues);
+        expQues = numQues;
+        testQues = s.getNumQues();
+
+        assertEquals(expQues, numQues);
+    }
+
+    @Test(expected = AppException.class)
+    public void zeroRangeConstrTest() throws AppException {
+        int range = 0;
+        Session s = new Session(range);
+    }
+
+    @Test(expected = AppException.class)
+    public void negRangeConstrTest() throws AppException {
+        int range = -5;
+        Session s = new Session(range);
+    }
+
+    @Test(expected = AppException.class)
+    public void zeroQuesConstrTest() throws AppException {
+        short numQues = 0;
+        Session s = new Session(numQues);
+    }
+
+    @Test(expected = AppException.class)
+    public void negQuesConstrTest() throws AppException {
+        int numQues = -5;
+        Session s = new Session(numQues);
+    }
+
 
 }
